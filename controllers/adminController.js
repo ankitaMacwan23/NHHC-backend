96391cacf60393d63525a233695998294af10129
ankitaMacwan23 <macwanankita33@gmail.com>
2025-04-12 20:38:37 +0530

    Add files via upload

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,6 @@
 const Patient = require('./../models/patient');
 const Caregivers = require('./../models/careGiver');
 
-<<<<<<< HEAD
 //--------------------for admin site functions--------------------------------------
 
 // Return patients in JSON format for React frontend
@@ -47,87 +46,10 @@ exports.getAllCareGivers = async (req, res, next) => {
   } catch (error) {
     console.error("Error fetching caregivers:", error);
     res.status(500).json({ error: "Failed to fetch caregivers" });
-=======
-exports.getAdminPage = (req,res,next) => {
-  
-    res.render('admin/admin',{pageTitle: 'Welcome to Admin page'});
-}
-
-//get patients list for admin for frontend
-/* exports.getPatients = (req, res, next) => {
-  Patient.find()
-    .then((registeredPatients) => {
-      //console.log(json.registeredPatients);
-      res.json({ patients: registeredPatients }); // Send JSON response
-    })
-    .catch((err) => {
-      res.status(500).json({ error: "Failed to fetch patients" });
-    });
-}; */
-
-exports.getPatients = (req, res, next) => {
-  Patient.find()
-    .then((registeredPatients) => {
-      res.render("patient/patients_list", { patients: registeredPatients, pageTitle: "Patients List" });
-    })
-    .catch((err) => {
-      res.status(500).send("Error retrieving patients");
-    });
-};
-
-exports.getCareGivers = async (req, res, next) => {
-  try {
-    const caregivers = await Caregivers.find();
-    res.render("caregiver/caregiver_list", { caregivers, pageTitle: "Caregivers List" });
-  } catch (error) {
-    console.error("Error fetching caregivers:", error);
-    res.status(500).send("Error retrieving caregivers");
-  }
-};
-
-exports.getNurses = async (req, res, next) => {
-  try {
-    const nurses = await Caregivers.find({ role: "Nurse" });
-    res.render("caregiver/nurses_list", { nurses, pageTitle: "Nurse List" });
-  } catch (error) {
-    console.error("Error fetching nurses:", error);
-    res.status(500).send("Error retrieving nurses");
-  }
-};
-
-exports.getDoctors = async (req, res, next) => {
-  try {
-    const doctors = await Caregivers.find({ role: "Doctor" });
-    res.render("caregiver/doctors_list", { doctors, pageTitle: "Doctor List" });
-  } catch (error) {
-    console.error("Error fetching doctors:", error);
-    res.status(500).send("Error retrieving doctors");
-  }
-};
-
-exports.getPhysios = async (req, res, next) => {
-  try {
-    const physios = await Caregivers.find({ role: "Physio" });
-    res.render("caregiver/physios_list", { physios, pageTitle: "Physio List" });
-  } catch (error) {
-    console.error("Error fetching physios:", error);
-    res.status(500).send("Error retrieving physios");
-  }
-};
-
-exports.getCleaners = async (req, res, next) => {
-  try {
-    const cleaners = await Caregivers.find({ role: "Cleaner" });
-    res.render("caregiver/cleaners_list", { cleaners, pageTitle: "Cleaner List" });
-  } catch (error) {
-    console.error("Error fetching cleaners:", error);
-    res.status(500).send("Error retrieving cleaners");
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
   }
 };
 
 
-<<<<<<< HEAD
 //--------------------------for frontend(App) Functions------------------------------
 
 
@@ -135,68 +57,3 @@ exports.getCleaners = async (req, res, next) => {
 
 
 
-=======
-exports.postApproveDoctor = async (req, res, next) => {
-  try {
-    const caregiverId = req.params.id;
-
-    // Update caregiver status to Approved
-    await Caregivers.findByIdAndUpdate(caregiverId, {
-      status: "Approved",
-    });
-
-    res.redirect("/admin/caregivers/doctor");
-  } catch (error) {
-    console.error("Error approving doctor:", error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.postApproveNurse = async (req, res, next) => {
-  try {
-    const caregiverId = req.params.id;
-
-    // Update caregiver status to Approved
-    await Caregivers.findByIdAndUpdate(caregiverId, {
-      status: "Approved",
-    });
-
-    res.redirect("/admin/caregivers/nurse");
-  } catch (error) {
-    console.error("Error approving doctor:", error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.postApprovePhysio = async (req, res, next) => {
-  try {
-    const caregiverId = req.params.id;
-
-    // Update caregiver status to Approved
-    await Caregivers.findByIdAndUpdate(caregiverId, {
-      status: "Approved",
-    });
-
-    res.redirect("/admin/caregivers/physio");
-  } catch (error) {
-    console.error("Error approving Physion:", error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.postApproveCleaner = async (req, res, next) => {
-  try {
-    const caregiverId = req.params.id;
-
-    // Update caregiver status to Approved
-    await Caregivers.findByIdAndUpdate(caregiverId, {
-      status: "Approved",
-    });
-
-    res.redirect("/admin/caregivers/cleaner");
-  } catch (error) {
-    console.error("Error approving Cleaner:", error);
-    res.status(500).send("Server Error");
-  }
-};
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Caregivers = require('../models/careGiver');
 const User = require('../models/user');
 const AdminUser = require('../models/adminUser');
@@ -6,9 +5,7 @@ const AdminUser = require('../models/adminUser');
 //--------------------------for admin site functions----------------------------------
 
 exports.getAdminLogin = async (req, res) => {
-  console.log("username is", req.body);
     const { username, password } = req.body;
-    console.log("username is", username);
     const user = await AdminUser.findOne({ username });
   
     if (!user) return res.status(401).json({ message: "Unauthorized" });
@@ -147,123 +144,4 @@ exports.postLogout = (req,res,next) => {
     res.redirect("/auth/login");
   })
 }
-=======
-const Caregivers = require('../models/careGiver');
-const User = require('../models/user');
-
-
-//mobile no saved after first time user install app
-exports.postSaveMobile = async (req, res) => {
-  try {
-    const { mobile } = req.body;
-    console.log(mobile);
-
-    if (!mobile) {
-      return res.status(400).json({ message: 'Mobile number is required' });
-    }
-
-    let user = await User.findOne({ mobile });
-
-    if (!user) {
-      user = new User({ mobile });
-      await user.save();
-    }
-
-    res.status(200).json({ message: 'Mobile saved', user });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
-  }
-};
-
-exports.getLogin = (req,res,next) => {
-    res.render('auth/login',{ pageTitle: 'Patient Added'});
-}
-
-// authController.js
-// const fast2sms = require('fast-two-sms'); // Commented out for now
-const OTPStore = {}; // Save OTPs temporarily
-
-exports.postLogin = async (req, res) => {
-  const { mobile, role } = req.body;
-
-  try {
-    const caregiver = await Caregivers.findOne({
-      contact: mobile,
-      role: role,
-      status: "Approved",
-    });
-
-    if (!caregiver) {
-      return res.status(401).json({
-        message: "Invalid credentials or account not approved by admin.",
-      });
-    }
-    //const otp = Math.floor(100000 + Math.random() * 900000).toString();
-
-    //OTPStore[mobile] = otp;
-
-    //console.log(`OTP for ${mobile} is ${otp}`); // For now
-
-    //return res.json({ success: true, message: "OTP sent successfully (check console)" });
-    return res.json({ success: true, caregiver });
-
-  } catch (err) {
-    console.error("Login error:", err);
-    res.status(500).json({ message: "Server error during login." });
-  }
-};
-
-exports.postVerifyOtp = async (req, res) => {
-  const { mobile, otp, role } = req.body;
-
-  const expectedOtp = OTPStore[mobile];
-
-  if (otp === expectedOtp) {
-    const caregiver = await Caregivers.findOne({ contact: mobile, role });
-
-    if (!caregiver) {
-      return res.status(404).json({ success: false, message: "Caregiver not found during OTP verification." });
-    }
-
-    delete OTPStore[mobile];
-    return res.json({ success: true, caregiver });
-  }
-
-  return res.status(400).json({ success: false, message: "Invalid OTP" });
-};
-
-exports.OTPStore = OTPStore;
-
-//without otp(uncomment later)
-/* exports.postLogin = async (req, res, next) => {
-  const { mobile, role } = req.body;
-
-  try {
-    const caregiver = await Caregivers.findOne({
-      contact: mobile,
-      role: role,
-      status: "Approved",
-    });
-
-    if (!caregiver) {
-      return res.status(401).json({
-        message: "Invalid credentials or account not approved by admin.",
-      });
-    }
-
-    res.status(200).json(caregiver);
-  } catch (err) {
-    console.error("Login error:", err);
-    res.status(500).json({ message: "Server error during login." });
-  }
-};
- */
-
-exports.postLogout = (req,res,next) => {
-  req.session.destroy(() => {
-    res.redirect("/auth/login");
-  })
-}
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
   
