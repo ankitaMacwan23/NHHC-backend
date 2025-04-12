0c9111e717109db1ee8241ea81fa639f4f7c8356
ankitaMacwan23 <macwanankita33@gmail.com>
2025-04-12 20:30:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,66 +1,24 @@
 require('dotenv').config();
-
 //core module
 const path = require('path');
 
 //External Modules
 const express = require('express');
-<<<<<<< HEAD
 const cors = require('cors');
+
 const bodyParser = require('body-parser');
 //const multer = require('multer');
-=======
-const bodyParser = require('body-parser');
-const multer = require('multer');
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
 const MongoDbStore = require('connect-mongodb-session')(session);
 
 // ✅ Get env vars before using them
-<<<<<<< HEAD
 const PORT = process.env.PORT || 3000;
-=======
-const PORT = process.env.PORT || 3001;
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
 const MONGO_DB_URL = process.env.MONGO_DB_URL;
 
 //Local Modules
 const storeRouter = require('./Routers/storeRouter');
-<<<<<<< HEAD
-const { patientRouter } = require('./Routers/patientRouter');
-const adminRouter = require('./Routers/adminRouter');
-const careGiverRouter = require('./Routers/careGiverRouter');
-const errorController = require('./controllers/errorController');
-const rootDir = require('./util/path-util');
-const authRouter = require('./Routers/authRouter');
-
-//const MONGO_DB_URL = "mongodb+srv://root:root@airbnb.n6vwm.mongodb.net/?retryWrites=true&w=majority&appName=airbnb";
-
-const app = express();
-
-app.use(cors({
-  origin: 'true', // handles localhost and production
-  credentials: true
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false,
-      httpOnly: true,
-      sameSite: "lax",
-    },
-  })
-);
-
-=======
 const {patientRouter} = require('./Routers/patientRouter');
 const adminRouter = require('./Routers/adminRouter');
 const careGiverRouter = require('./Routers/careGiverRouter');
@@ -70,7 +28,9 @@ const errorController =  require('./controllers/errorController');
 const rootDir =  require('./util/path-util');
 const authRouter = require('./Routers/authRouter');
 
-const storage = multer.diskStorage({
+//const MONGO_DB_URL ="mongodb+srv://root:root@airbnb.n6vwm.mongodb.net/?retryWrites=true&w=majority&appName=airbnb";
+
+/* const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
   },
@@ -85,41 +45,43 @@ const fileFilter = (req, file, cb) => {
   }else{
     cb(null,false);
   }
-};
+}; */
 
 const app = express();
-
+app.use(cors({
+  origin: 'true',
+  credentials: true
+}));
 app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false, // use true only if using HTTPS
+      httpOnly: true,
+      sameSite: "lax",
+    },
+  })
+);
+
 app.use(express.urlencoded({ extended: true })); 
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-<<<<<<< HEAD
-// static files from legacy 'public' folder
-//app.use(express.static(path.join(rootDir, "public")));
-=======
 app.use(express.static(path.join(rootDir, "public")));
 
-app.use(bodyParser.urlencoded());
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
+//app.use(bodyParser.urlencoded());
 
 const store = new MongoDbStore({
   uri: MONGO_DB_URL,
   collection: 'sessions'
 });
 
-<<<<<<< HEAD
-=======
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  store: store,
-}));
 
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
 app.use(flash());
 
 // Make flash messages available in all views
@@ -129,44 +91,32 @@ app.use((req, res, next) => {
   next();
 });
 
-<<<<<<< HEAD
-app.use((req, res, next) => {
-  res.locals.session = req.session;
-  next();
-});
-
-// to upload patient medication doc, caregivers's adhar and document
-/* app.use(
-=======
 app.use((req,res,next) => {
   res.locals.session = req.session;
   next();
 })
 
 //to upload patient medication doc, caregivers's adhar and document
-app.use(
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
+/* app.use(
   multer({ storage, fileFilter }).fields([
     { name: 'medicationPhotoUrl', maxCount: 1 },
     { name: 'caregiver_aadhar', maxCount: 1 },
     { name: 'caregiver_document', maxCount: 1 }
   ])
-<<<<<<< HEAD
-); */
-=======
 );
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
-
+ */
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/auth", authRouter);
-<<<<<<< HEAD
+
 app.use(storeRouter);
+
 app.use("/patient", patientRouter);
+
 app.use("/admin", adminRouter);
+
 app.use("/caregiver", careGiverRouter);
 
-// ✅ Serve React frontend (dist folder)
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
@@ -176,34 +126,14 @@ app.get('*', (req, res) => {
 
 app.use(errorController.get404);
 
-=======
+//const PORT = 3000;
 
-app.use(storeRouter);
-
-app.use("/patient", patientRouter);
-
-app.use("/admin", adminRouter);
-
-app.use("/caregiver", careGiverRouter);
-
-app.use(errorController.get404);
-
-
-
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
 mongoose.connect(MONGO_DB_URL).then(() => {
   console.log('Connected to Mongoose');
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
-<<<<<<< HEAD
-  });
-}).catch(err => {
-  console.log('Error occur while connecting to mongoose', err);
-});
-=======
   })
 
 }).catch(err => {
   console.log('Error occur while connecting to mongoose', err);
 })
->>>>>>> 4c41389958edadc7fa3029204947fa161358c254
