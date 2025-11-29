74283073a8ad8b72dc0e684ddd53a89c0a1e54a4
ankitaMacwan23 <macwanankita33@gmail.com>
2025-11-29 13:26:15 +0530

    Add files via upload

--- middleware/caregiverUpload.js
+++ middleware/caregiverUpload.js
@@ -3,8 +3,5 @@ const storage = multer.memoryStorage();
 
 module.exports = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
-}).fields([
-  { name: "aadhar", maxCount: 1 },
-  { name: "certificate", maxCount: 1 }
-]);
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
